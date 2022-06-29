748a849a4d49b87206d11f0f934b852b3f4b592e
Roger Rangel <roger.rangel.ee@gmail.com>
2022-06-29 16:51:31 +0200

    Creating the Faucet Functionality Using the Shared Keyword

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react"
+import { toke } from "../../../declarations/token"
+import { token } from "../../../declarations/token/index";
 
 function Faucet() {
 
-  async function handleClick(event) {
+  const [isDisabled, setDisable] = useState(false); 
+  const [buttonText, setText] = useState("Gimme plz!")
 
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    setText(result);
+    // setDisable(false);
   }
 
   return (
@@ -16,8 +24,8 @@ function Faucet() {
       </h2>
       <label>Get your free Segenie tokens here! Claim 10,000 SEGEN coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
